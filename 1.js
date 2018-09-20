8adc5b97846766fdaa0423cf7eeb3e8ce0eb8763
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2018-09-20 11:18:58 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 let url = "https://lunch.thechels.uk/locations.json"
 let req = new Request(url)
 let json = await req.loadJSON()
-let output = console.log(Object.keys(json)[Math.floor(Math.random()*Object.keys(json).length)]);
+let output = Object.keys(json)[Math.floor(Math.random()*Object.keys(json).length)];
 console.log(output)
